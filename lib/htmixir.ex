edd33d71ab36f08fd362b3410531cbf20a7335bf
Dunya Kirkali <dunya.kirkali@booking.com>
2024-01-01 10:33:24 +0100

    Initial commit

---
+++ lib/htmixir.ex
@@ -0,0 +1,18 @@
+defmodule Htmixir do
+  @moduledoc """
+  Documentation for `Htmixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Htmixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
